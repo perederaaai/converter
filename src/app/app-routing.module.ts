a0be6c35264f99db54fecf05a18b511a406a1409
perederaaai <igor.peredera@ukr.net>
2024-01-11 14:09:37 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './converter-module/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+      path: '',
+      loadChildren: () => import('./converter-module/converter-module').then(m => m.ConverterModule)
+    },
+    {
+      path: '**',
+      component: ErrorPageComponent
+    }
+
+  ]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomInputComponent } from './shared/custom-input/custom-input.component';
+import { CustomSelectComponent } from './shared/custom-select/custom-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomInputComponent,
+    CustomSelectComponent
   ],
   imports: [
     BrowserModule,
